75fc25a73a73728d264fd8e4b766d734d17d7a66
El Thomas <thomas.folmer@outlook.fr>
2017-11-27 11:35:20 +0100

    First draft

--- DocContact/DocContact/ViewController.swift
+++ DocContact/DocContact/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func connexionAction(_ sender: Any) {
+        
+    }
 
 }
 
