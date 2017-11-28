f38b469ed30dcd8d50515f021112df4e40788129
Unknown <guillaume.lazaro@gmail.com>
2017-11-28 16:52:29 +0100

    Gravatar
    Add Hash MD5 and Gravatar from an email address

---
+++ DocContact/DocContact/DocContact-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  DocContact-Bridging-Header.h
+//  DocContact
+//
+//  Created by Guillaume Lazaro on 28/11/2017.
+//  Copyright © 2017 Cuba Libre. All rights reserved.
+//
+
+#ifndef DocContact_Bridging_Header_h
+#define DocContact_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h> //Fichier .h pour créer le MD5
+
+#endif /* DocContact_Bridging_Header_h */
