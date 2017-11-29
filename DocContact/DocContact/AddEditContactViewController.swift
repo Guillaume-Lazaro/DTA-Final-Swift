6a28b8051535d0fec6380e0de1a1d53d3fd29415
Unknown <louis.benjamin@live.fr>
2017-11-29 18:01:59 +0100

    fields are prefilled on modify and modify-delete works

--- DocContact/DocContact/AddEditContactViewController.swift
+++ DocContact/DocContact/AddEditContactViewController.swift
@@ -62,6 +62,15 @@ class AddEditContactViewController: UIViewController, UIPickerViewDataSource, UI
             self.title = "Edition du contact"
             deleteButton.isEnabled = true
             deleteButton.isHidden = false
+            nameTextField.text = contact?.lastName
+            firstNameTextField.text = contact?.firstName
+            phoneTextField.text = contact?.phone
+            emailTextField.text = contact?.email
+            guard let emergency = contact?.isEmergencyUser else{
+                return
+            }
+            emergencyUserSwitch.setOn(emergency, animated: false)
+            pickerTextField.text = contact?.profile
             //TODO: Pré-remplir les données
         } else {
             self.title = "Ajouter un contact"
@@ -148,8 +157,11 @@ class AddEditContactViewController: UIViewController, UIPickerViewDataSource, UI
             if !self.isInEditionMode{
                 netProvider.createContact(phone: phone, firstname: firstname, lastname: name, mail: mail, profile: profile, gravatar: gravatar, emergency: emergency, token: token)
             } else {
+                guard let id = contact?.id else{
+                    return
+                }
                 // TODO : gérer via l'id du contact en cours (pas en dur)
-                netProvider.updateContact(phone: phone, firstname: firstname, lastname: name, mail: mail, profile: profile, gravatar: gravatar, emergency: emergency,id:"5a1ebbe3bcee2b07a5b5b575", token: token)
+                netProvider.updateContact(phone: phone, firstname: firstname, lastname: name, mail: mail, profile: profile, gravatar: gravatar, emergency: emergency,id:id, token: token)
             }
             
             let contactVC = ContactListTableViewController(nibName: nil, bundle: nil)
