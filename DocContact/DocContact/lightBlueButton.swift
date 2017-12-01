95aa9359e9d8b9c291a6209e1f7509dd6f5d9eda
El Thomas <thomas.folmer@outlook.fr>
2017-12-01 12:29:25 +0100

    Revert "bugfix"
    This reverts commit 64a816e86de5120662023f146d70fbf7a0588a9e.

--- DocContact/DocContact/lightBlueButton.swift
+++ DocContact/DocContact/lightBlueButton.swift
@@ -1,5 +1,5 @@
 //
-//  LightBlueButton.swift
+//  lightBlueButton.swift
 //  DocContact
 //
 //  Created by Thomas on 01/12/2017.
@@ -9,16 +9,11 @@
 import Foundation
 import UIKit
 
-
-class LightBlueButton : UIButton{
-    
+class lightBlueButton : UIButton{
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = self.frame.width/2
         self.layer.cornerRadius = self.frame.height/2
-        self.backgroundColor = UIColor.init(red: 114.0/255.0, green: 147.0/255.0, blue: 220.0/255.0, alpha: 1.0)
-        self.setTitleColor( UIColor.init(red: 65.0/255.0, green: 75.0/255.0, blue: 168.0/255.0, alpha: 1.0), for: .highlighted)
-        
+        self.backgroundColor = UIColor.init(red: 65, green: 75, blue: 168, alpha: 1)
     }
-    
 }
