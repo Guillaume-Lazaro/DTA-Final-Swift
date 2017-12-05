e1fc3eae631bef192566cbcad2dca57f9a5b42c9
drRoux <lerouxvincent76000@gmail.com>
2017-12-05 11:59:25 +0100

    Fixing saving of the context on adding contacts and deleting current user

--- DocContact/DocContact/Providers/manageDBProvider.swift
+++ DocContact/DocContact/Providers/manageDBProvider.swift
@@ -12,132 +12,126 @@ import CoreData
 private let sharedManageDbProvider = ManageDbProvider()
 
 class ManageDbProvider{
-    let netProvider = NetworkProvider.sharedInstance
-    class var sharedInstance: ManageDbProvider {
-        return sharedManageDbProvider
-    }
-    
-    func createCoreDataUser(userJson: [String: String]){
-        let appDelegate = netProvider.persistentContainer
-        let context = appDelegate.viewContext
-        let user = User(context: context)
-        user.lastName = userJson["lastName"]
-        user.firstName = userJson["firstName"]
-        user.phone = userJson["phone"]
-        user.email = userJson["email"]
-        user.profile = userJson["profile"]
-        user.token = netProvider.token
-        print("user ajouté à la base locale")
-        do{
-            if context.hasChanges{
-                try context.save()
-            }
-        }catch{
-            print(error)
-        }
-        print("L'utilisateur a été ajouté à la base")
-    }
-    func getUser()->User?{
-        let fetchRequestUser = NSFetchRequest<User>(entityName : "User")
-        let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        do {
-            let fetchedUser = try managedContext.fetch(fetchRequestUser)
-           // print(fetchedUser.first?.lastName)
-            return fetchedUser.first
-        } catch {
-            fatalError("Failed to fetch employees: \(error)")
-        }
-    }
-    
-    func deleteUsersFromCoreData(){
-        let fetchRequest = NSFetchRequest<User>(entityName: "User")
-        let netProvider = NetworkProvider.sharedInstance
-        let appDelegate = netProvider.persistentContainer
-        let context = appDelegate.viewContext
-        let contacts : [User] = try! context.fetch(fetchRequest)
-        
-        contacts.forEach { (user) in
-            context.delete(user)
-            do{
-                if context.hasChanges{
-                    try context.save()
-                }
-            }catch{
-                print(error)
-            }
-        }
-    }
-    
-    
-    func fillCoreDataWithContacts(json: [[String : Any]]){                    // Passed in data from remote DB
-        let sort = NSSortDescriptor(key: "lastName", ascending: true)
-        let fetchRequest = NSFetchRequest<Contact>(entityName: "Contact")
-        fetchRequest.sortDescriptors = [sort]
-        let appDelegate = netProvider.persistentContainer
-        let context = appDelegate.viewContext
-        // create
-        for jsonPerson in json{
-            let contact = Contact(context: context)
-            // contact.user = context
-            contact.lastName = jsonPerson["lastName"] as? String
-            contact.firstName = jsonPerson["firstName"] as? String
-            contact.email = jsonPerson["email"] as? String
-            contact.phone = jsonPerson["phone"] as? String
-            contact.profile = jsonPerson["profile"] as? String
-            contact.gravatar = jsonPerson["gravatar"] as? String
-            contact.id = jsonPerson["_id"] as? String
-
-            guard let emergencyUser = jsonPerson["isEmergencyUser"] as? Int else{
-                print("ERROR")
-                return
-            }
-            if(emergencyUser == 0){
-                contact.isEmergencyUser = false
-            }else{
-                contact.isEmergencyUser = true
-            }
-            guard let familinkUser = jsonPerson["isFamilinkUser"] as? Int else{
-                print("ERROR")
-                return
-            }
-            if(familinkUser == 0){
-                contact.isFamilinkUser = false
-            }else{
-                contact.isFamilinkUser = true
-            }
-            
-        
-            do{
-                if context.hasChanges{
-                    try context.save()
-                }
-            }catch{
-                print(error)
-            }
-            
-        }
-        
-        print("Les contacts ont bien été ajoutés à la base de données locale")
-    }
-    
-    func wipeContacts(){
-        let fetchRequest = NSFetchRequest<Contact>(entityName: "Contact")
-        let netProvider = NetworkProvider.sharedInstance
-        let appDelegate = netProvider.persistentContainer
-        let context = appDelegate.viewContext
-        let contacts : [Contact] = try! context.fetch(fetchRequest)
-        
-        contacts.forEach { (contact) in
-            context.delete(contact)
-            do{
-                if context.hasChanges{
-                    try context.save()
-                }
-            }catch{
-                print(error)
-            }
-        }        
-    }
+	let netProvider = NetworkProvider.sharedInstance
+	class var sharedInstance: ManageDbProvider {
+		return sharedManageDbProvider
+	}
+	
+	func createCoreDataUser(userJson: [String: String]){
+		let appDelegate = netProvider.persistentContainer
+		let context = appDelegate.viewContext
+		let user = User(context: context)
+		user.lastName = userJson["lastName"]
+		user.firstName = userJson["firstName"]
+		user.phone = userJson["phone"]
+		user.email = userJson["email"]
+		user.profile = userJson["profile"]
+		user.token = netProvider.token
+		print("user ajouté à la base locale")
+		do{
+			if context.hasChanges{
+				try context.save()
+			}
+		}catch{
+			print(error)
+		}
+		print("L'utilisateur a été ajouté à la base")
+	}
+	func getUser()->User?{
+		let fetchRequestUser = NSFetchRequest<User>(entityName : "User")
+		let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+		do {
+			let fetchedUser = try managedContext.fetch(fetchRequestUser)
+			// print(fetchedUser.first?.lastName)
+			return fetchedUser.first
+		} catch {
+			fatalError("Failed to fetch employees: \(error)")
+		}
+	}
+	
+	func deleteUsersFromCoreData(){
+		
+		let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+		
+		if let user = getUser() {
+			managedContext.delete(user)
+		}
+		
+		do{
+			if managedContext.hasChanges{
+				try managedContext.save()
+			}
+		} catch {
+			print(error)
+		}
+	}
+	
+	
+	func fillCoreDataWithContacts(json: [[String : Any]]){                    // Passed in data from remote DB
+		let sort = NSSortDescriptor(key: "lastName", ascending: true)
+		let fetchRequest = NSFetchRequest<Contact>(entityName: "Contact")
+		fetchRequest.sortDescriptors = [sort]
+		let appDelegate = netProvider.persistentContainer
+		let context = appDelegate.viewContext
+		// create
+		for jsonPerson in json{
+			let contact = Contact(context: context)
+			// contact.user = context
+			contact.lastName = jsonPerson["lastName"] as? String
+			contact.firstName = jsonPerson["firstName"] as? String
+			contact.email = jsonPerson["email"] as? String
+			contact.phone = jsonPerson["phone"] as? String
+			contact.profile = jsonPerson["profile"] as? String
+			contact.gravatar = jsonPerson["gravatar"] as? String
+			contact.id = jsonPerson["_id"] as? String
+			
+			guard let emergencyUser = jsonPerson["isEmergencyUser"] as? Int else{
+				print("ERROR")
+				return
+			}
+			if(emergencyUser == 0){
+				contact.isEmergencyUser = false
+			}else{
+				contact.isEmergencyUser = true
+			}
+			guard let familinkUser = jsonPerson["isFamilinkUser"] as? Int else{
+				print("ERROR")
+				return
+			}
+			if(familinkUser == 0){
+				contact.isFamilinkUser = false
+			}else{
+				contact.isFamilinkUser = true
+			}
+			
+		}
+		
+		do{
+			if context.hasChanges{
+				try context.save()
+			}
+		}catch{
+			print(error)
+		}
+		
+		
+		
+		print("Les contacts ont bien été ajoutés à la base de données locale")
+	}
+	
+	func wipeContacts(){
+		let fetchRequest = NSFetchRequest<Contact>(entityName: "Contact")
+		let netProvider = NetworkProvider.sharedInstance
+		let appDelegate = netProvider.persistentContainer
+		let context = appDelegate.viewContext
+		let contacts : [Contact] = try! context.fetch(fetchRequest)
+		
+		contacts.forEach { (contact) in
+			context.delete(contact)
+		}
+		
+		
+	}
 }
 
- 
+
